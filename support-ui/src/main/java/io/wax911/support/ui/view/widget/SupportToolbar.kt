a727bbdac853e699be7b73cc0de75ce939273730
Maxwell <mxt.developer@gmail.com>
2019-04-14 12:17:48 +0200

    Support UI Module

---
+++ support-ui/src/main/java/io/wax911/support/ui/view/widget/SupportToolbar.kt
@@ -0,0 +1,24 @@
+package io.wax911.support.ui.view.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.Toolbar
+import io.wax911.support.core.view.contract.CustomView
+
+class SupportToolbar : Toolbar, CustomView {
+
+    constructor(context: Context?)
+            : super(context) { onInit() }
+    constructor(context: Context?, attrs: AttributeSet?)
+            : super(context, attrs) { onInit() }
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int)
+            : super(context, attrs, defStyleAttr) { onInit() }
+
+    /**
+     * Callable in view constructors to perform view inflation and
+     * additional attribute initialization
+     */
+    override fun onInit() {
+
+    }
+}
