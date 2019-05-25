252090efec396dddfb1da3a3087d3a3b4d563051
Maxwell <mxt.developer@gmail.com>
2019-05-25 10:56:17 +0200

    Implementation of .equal & .hashCode

--- core/src/main/java/io/wax911/sample/core/model/movie/Movie.kt
+++ core/src/main/java/io/wax911/sample/core/model/movie/Movie.kt
@@ -29,4 +29,33 @@ data class Movie(
     override val language: String?,
     override val genres: List<String>?,
     override val certification: String?
-): TraktEntity, Parcelable
+): TraktEntity, Parcelable {
+
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            is TraktEntity -> other.id == id
+            else -> super.equals(other)
+        }
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + ids.hashCode()
+        result = 31 * result + tagline.hashCode()
+        result = 31 * result + (released?.hashCode() ?: 0)
+        result = 31 * result + title.hashCode()
+        result = 31 * result + year
+        result = 31 * result + (overview?.hashCode() ?: 0)
+        result = 31 * result + (runtime ?: 0)
+        result = 31 * result + (country?.hashCode() ?: 0)
+        result = 31 * result + (trailer?.hashCode() ?: 0)
+        result = 31 * result + (homepage?.hashCode() ?: 0)
+        result = 31 * result + (rating?.hashCode() ?: 0)
+        result = 31 * result + (votes ?: 0)
+        result = 31 * result + (updated_at?.hashCode() ?: 0)
+        result = 31 * result + (language?.hashCode() ?: 0)
+        result = 31 * result + (genres?.hashCode() ?: 0)
+        result = 31 * result + (certification?.hashCode() ?: 0)
+        return result
+    }
+}
