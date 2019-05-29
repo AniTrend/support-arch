643491b8ad10bcbd9819c366423cb7c9a8a13d69
Maxwell <mxt.developer@gmail.com>
2019-05-29 16:10:26 +0200

    Improved Abstraction of Data Sources & Data Mappers
    Data source and mapper for both paging and non paging requests

--- support-data/src/main/java/io/wax911/support/data/repository/SupportRepository.kt
+++ support-data/src/main/java/io/wax911/support/data/repository/SupportRepository.kt
@@ -2,23 +2,14 @@ package io.wax911.support.data.repository
 
 import io.wax911.support.data.factory.contract.IRetrofitFactory
 import io.wax911.support.data.repository.contract.ISupportRepository
-import io.wax911.support.extension.util.SupportConnectivityHelper
-import org.koin.core.inject
-import timber.log.Timber
+import kotlinx.coroutines.SupervisorJob
 
 abstract class SupportRepository<V>: ISupportRepository<V> {
 
     protected abstract val retroFactory: IRetrofitFactory
 
-    override val supportConnectivityHelper: SupportConnectivityHelper by inject()
-
-    private fun isConnectedToActiveNetwork(): Boolean {
-        return try {
-            val connected = supportConnectivityHelper.connectedStatus.value
-            connected ?: false
-        } catch (e: Exception) {
-            Timber.e(e, "Failed to check internet connectivity")
-            false
-        }
-    }
+    /**
+     * Requires an instance of [kotlinx.coroutines.Job] or [kotlinx.coroutines.SupervisorJob]
+     */
+    override val supervisorJob = SupervisorJob()
 }
--- support-data/src/main/java/io/wax911/support/data/source/SupportDataSource.kt
+++ support-data/src/main/java/io/wax911/support/data/source/SupportDataSource.kt
@@ -1,44 +1,46 @@
 package io.wax911.support.data.source
 
 import android.os.Bundle
-import androidx.lifecycle.LiveData
-import androidx.paging.PagedList
-import androidx.paging.PagingRequestHelper
-import androidx.paging.extension.createStatusLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.room.RoomDatabase
+import io.wax911.support.data.model.NetworkState
 import io.wax911.support.data.source.contract.ISupportDataSource
-import io.wax911.support.data.util.SupportDataKeyStore
-import io.wax911.support.data.util.pagination.SupportPagingHelper
-import io.wax911.support.extension.util.SupportExtKeyStore
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+import io.wax911.support.extension.util.SupportConnectivityHelper
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import org.koin.core.inject
 
-abstract class SupportDataSource<T>(
-    protected val bundle: Bundle
-) : PagedList.BoundaryCallback<T>(), ISupportDataSource {
+/**
+ * A non paging dependant data source for single resource requests
+ *
+ * @param parentCoroutineJob parent coroutine from something that is lifecycle aware,
+ * this enables us to cancels jobs automatically when the parent is also canceled
+ */
+abstract class SupportDataSource(
+    parentCoroutineJob: Job? = null
+) : ISupportDataSource {
+
+    /**
+     * Requires an instance of [kotlinx.coroutines.Job] or [kotlinx.coroutines.SupervisorJob]
+     */
+    override val supervisorJob: Job = SupervisorJob(parentCoroutineJob)
+
+    /**
+     * Connectivity helper utility with live data observable capabilities
+     */
+    override val supportConnectivityHelper by inject<SupportConnectivityHelper>()
+
+    override val networkState = MutableLiveData<NetworkState>()
 
     protected abstract val databaseHelper: RoomDatabase
 
-    val pagingRequestHelper = PagingRequestHelper(IO_EXECUTOR)
-
-    val networkState = pagingRequestHelper.createStatusLiveData()
-
-    protected val supportPagingHelper: SupportPagingHelper?
-        get() = bundle.getParcelable(SupportExtKeyStore.key_pagination)
-
-    interface IDataObservable <O> {
-
-        /**
-         * Returns the appropriate observable which we will monitor for updates,
-         * common implementation may include but not limited to returning
-         * data source live data for a database
-         *
-         * @param bundle request params, implementation is up to the developer
-         */
-        fun observerOnLiveDataWith(bundle: Bundle): LiveData<O>
-    }
-
-    companion object {
-        val IO_EXECUTOR: ExecutorService = Executors.newSingleThreadExecutor()
+    /**
+     * Handles the requesting data from a the network source and informs the
+     * network state that it is in the loading state
+     *
+     * @param bundle request parameters or more
+     */
+    open fun startRequestForType(bundle: Bundle) {
+        networkState.postValue(NetworkState.LOADING)
     }
 }
