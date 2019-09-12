c31274b902da3a5fac501158bba10c76d12358d9
Maxwell <mxt.developer@gmail.com>
2019-09-12 10:54:19 +0200

    Relax Enforcement on Use Case Implementation

--- support-domain/src/main/kotlin/co/anitrend/arch/domain/usecases/ISupportUseCase.kt
+++ support-domain/src/main/kotlin/co/anitrend/arch/domain/usecases/ISupportUseCase.kt
@@ -1,11 +1,11 @@
-package co.anitrend.arch.domain.usecases.core
+package co.anitrend.arch.domain.usecases
 
 import co.anitrend.arch.domain.common.IUseCase
 
 /**
  * Use case representative for non coroutine related context
  */
-interface ISupportCoreUseCase<P, R> : IUseCase {
+interface ISupportUseCase<P, R> : IUseCase {
 
     /**
      * Solves a given use case in the implementation target
