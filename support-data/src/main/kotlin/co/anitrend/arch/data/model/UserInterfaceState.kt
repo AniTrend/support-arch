5ce08d9e7945a3d3a2be9d312590931b93be36d8
Maxwell <mxt.developer@gmail.com>
2019-09-12 11:26:31 +0200

    Improve Creation of UI State with Builder Functions
    Adds an internal constructor to `UserInterfaceState` to avoid repositories from knowing about how some states are created or handled

--- support-data/src/main/kotlin/co/anitrend/arch/data/model/UserInterfaceState.kt
+++ support-data/src/main/kotlin/co/anitrend/arch/data/model/UserInterfaceState.kt
@@ -1,6 +1,11 @@
 package co.anitrend.arch.data.model
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import co.anitrend.arch.data.source.contract.IDataSource
+import co.anitrend.arch.data.source.core.contract.ICoreDataSource
+import co.anitrend.arch.data.source.paging.contract.IPagingDataSource
 import co.anitrend.arch.domain.common.IUserInterfaceState
 import co.anitrend.arch.domain.entities.NetworkState
 
@@ -9,10 +14,59 @@ import co.anitrend.arch.domain.entities.NetworkState
  *
  * @param model LiveData for the UI to observe
  */
-data class UserInterfaceState<T>(
+data class UserInterfaceState<T> internal constructor(
     val model: LiveData<T>,
     override val networkState: LiveData<NetworkState>,
     override val refreshState: LiveData<NetworkState>,
     override val refresh: () -> Unit,
     override val retry: () -> Unit
-): IUserInterfaceState<LiveData<NetworkState>>
+): IUserInterfaceState<LiveData<NetworkState>> {
+
+    companion object {
+        fun <T> create(
+            model: LiveData<T>,
+            source: IPagingDataSource
+        ) : UserInterfaceState<T> {
+            val refreshTrigger = MutableLiveData<Unit>()
+            val refreshState = Transformations.switchMap(refreshTrigger) {
+                MutableLiveData<NetworkState>()
+            }
+
+            return UserInterfaceState(
+                model = model,
+                networkState = source.networkState,
+                refreshState = refreshState,
+                refresh = {
+                    source.invalidateAndRefresh()
+                    refreshTrigger.value = null
+                },
+                retry = {
+                    source.retryRequest()
+                }
+            )
+        }
+
+        fun <T> create(
+            model: LiveData<T>,
+            source: ICoreDataSource
+        ) : UserInterfaceState<T> {
+            val refreshTrigger = MutableLiveData<Unit>()
+            val refreshState = Transformations.switchMap(refreshTrigger) {
+                MutableLiveData<NetworkState>()
+            }
+
+            return UserInterfaceState(
+                model = model,
+                networkState = source.networkState,
+                refreshState = refreshState,
+                refresh = {
+                    source.invalidateAndRefresh()
+                    refreshTrigger.value = null
+                },
+                retry = {
+                    source.retryRequest()
+                }
+            )
+        }
+    }
+}
