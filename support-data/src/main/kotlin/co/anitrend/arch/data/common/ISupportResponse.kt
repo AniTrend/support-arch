a621f6a44b595f64a3dbcc9f26ba0edd13a86d5f
Maxwell <mxt.developer@gmail.com>
2019-09-14 20:13:52 +0200

    Refactor Response Handlers: Separation of Concerns

---
+++ support-data/src/main/kotlin/co/anitrend/arch/data/common/ISupportResponse.kt
@@ -0,0 +1,26 @@
+package co.anitrend.arch.data.common
+
+import androidx.lifecycle.MutableLiveData
+import co.anitrend.arch.domain.entities.NetworkState
+
+/**
+ * Contract mapper for wrapping future requests, commonly in the form of a
+ * [kotlinx.coroutines.Deferred] or [retrofit2.Call]
+ *
+ * @since v1.1.0
+ */
+interface ISupportResponse<in RESOURCE, out RESPONSE> {
+
+    /**
+     * Response handler for coroutine contexts which need to observe [NetworkState]
+     *
+     * @param resource awaiting execution
+     * @param networkState for the deferred result
+     *
+     * @return resource fetched if present
+     */
+    suspend operator fun invoke(
+        resource: RESOURCE,
+        networkState: MutableLiveData<NetworkState>
+    ) : RESPONSE?
+}
