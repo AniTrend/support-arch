84353018bbdd0431d34759138d2d4949cc565e17
Maxwell <mxt.developer@gmail.com>
2020-01-02 11:36:02 +0200

    Replace property view with kotlin synthetics

--- support-ui/src/main/kotlin/co/anitrend/arch/ui/recycler/common/SupportFooterErrorViewHolder.kt
+++ support-ui/src/main/kotlin/co/anitrend/arch/ui/recycler/common/SupportFooterErrorViewHolder.kt
@@ -8,6 +8,7 @@ import co.anitrend.arch.ui.recycler.holder.event.ItemClickListener
 import co.anitrend.arch.ui.util.SupportStateLayoutConfiguration
 import co.anitrend.arch.ui.view.text.SingleLineTextView
 import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.support_layout_state_footer_error.view.*
 
 /**
  * Footer view holder for representing loading errors
@@ -21,9 +22,6 @@ class SupportFooterErrorViewHolder<T>(
     private val configuration: SupportStateLayoutConfiguration
 ) : SupportViewHolder<T>(view.rootView) {
 
-    private val stateText = view.findViewById<SingleLineTextView>(R.id.stateFooterErrorText)
-    private val stateAction = view.findViewById<MaterialButton>(R.id.stateFooterErrorAction)
-
     /**
      * Load images, text, buttons, etc. in this method from the given parameter
      *
@@ -31,18 +29,18 @@ class SupportFooterErrorViewHolder<T>(
      */
     override fun invoke(model: T?) {
         if (networkState is NetworkState.Error)
-            stateText.text = networkState.message
-        stateAction.setText(configuration.retryAction)
-        stateAction.setOnClickListener(retryAction)
+            view.stateFooterErrorText.text = networkState.message
+        view.stateFooterErrorAction.setText(configuration.retryAction)
+        view.stateFooterErrorAction.setOnClickListener(retryAction)
     }
 
     /**
-     * If any image views are used within the view holder, clear any pending async requests
-     * by using [com.bumptech.glide.RequestManager.clear]
-     *
-     * @see com.bumptech.glide.Glide
+     * Clear or unbind any references the views might be using, e.g. image loading
+     * libraries, data binding, callbacks e.t.c
      */
-    override fun onViewRecycled() {}
+    override fun onViewRecycled() {
+
+    }
 
     /**
      * Handle any onclick events from our views, optionally you can call
