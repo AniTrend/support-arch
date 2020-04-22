744fede9c9aa37203fb376b262b2298b869d41d6
Maxwell <mxt.developer@gmail.com>
2020-04-22 12:35:06 +0200

    Implement temporary members to selectioin mode

--- support-recycler/src/main/kotlin/co/anitrend/arch/recycler/action/SupportSelectionMode.kt
+++ support-recycler/src/main/kotlin/co/anitrend/arch/recycler/action/SupportSelectionMode.kt
@@ -1,9 +1,11 @@
 package co.anitrend.arch.recycler.action
 
-import co.anitrend.arch.core.presenter.contract.ISupportPresenter
+import android.view.View
 import co.anitrend.arch.recycler.action.contract.ISupportSelectionListener
 import co.anitrend.arch.recycler.action.contract.ISupportSelectionMode
+import co.anitrend.arch.recycler.action.decorator.ISelectionDecorator
 import co.anitrend.arch.recycler.model.RecyclerItem
+import co.anitrend.arch.recycler.model.contract.IRecyclerItem
 
 /**
  * A helper class for triggering action mode for
@@ -11,11 +13,9 @@ import co.anitrend.arch.recycler.model.RecyclerItem
  *
  * @since v1.3.0
  */
-class SupportSelectionMode<I: RecyclerItem>(
-    private val selectionListener: ISupportSelectionListener?,
-    private val presenter: ISupportPresenter?
-) : ISupportSelectionMode<I> {
-
+class SupportSelectionMode<ID>(
+    private val selectionListener: ISupportSelectionListener?
+) : ISupportSelectionMode<ID> {
     /**
      * Clears all selected items in the current context and alternative stop the current action mode
      */
@@ -24,9 +24,54 @@ class SupportSelectionMode<I: RecyclerItem>(
     }
 
     /**
-     * Checks if item exists in the current selection
+     * Selects all defined items and reflects changes on the action mode
+     *
+     * @param selection items which can be selected
      */
-    override fun containsItem(item: I?): Boolean {
+    override fun selectAllItems(selection: List<ID>) {
         TODO("Not yet implemented")
     }
+
+    /**
+     * Defines whether or not this current object can be consumed as a primary click action,
+     * or if in action mode should be selected or deselected.
+     *
+     * @param id item that has been clicked
+     * @param view the view holder parent of the clicked item
+     *
+     * @return true if not currently in action mode otherwise false
+     */
+    override fun isSelectionClickable(
+        view: View,
+        decorator: ISelectionDecorator?,
+        id: ID
+    ): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * Defines whether or not this current object can be consumed as a primary long click,
+     * or if in action mode should start the action mode and also select or deselect the item.
+     *
+     * @param id item that has been clicked
+     * @param view the view holder parent of the clicked item
+     *
+     * @return true if in action mode to inform long click listener that the
+     * we have consumed the event, otherwise false
+     */
+    override fun isLongSelectionClickable(
+        view: View,
+        decorator: ISelectionDecorator?,
+        id: ID
+    ): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     * Checks if item exists in the current selection
+     */
+    override fun containsItem(id: ID): Boolean {
+        TODO("Not yet implemented")
+    }
+
 }
