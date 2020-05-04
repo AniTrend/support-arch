8c7c03a5a4a05c4ec460564255bb6a85518a4666
Maxwell <mxt.developer@gmail.com>
2020-05-04 16:58:15 +0200

    Remove deprecation warning and updated documentation

--- support-data/src/main/kotlin/co/anitrend/arch/data/mapper/contract/ISupportResponseMapper.kt
+++ support-data/src/main/kotlin/co/anitrend/arch/data/mapper/contract/ISupportResponseMapper.kt
@@ -5,18 +5,10 @@ package co.anitrend.arch.data.mapper.contract
  *
  * @since v1.1.0
  */
-@Deprecated(
-    "Consider using converter instead for better coverage",
-    ReplaceWith(
-        expression = "SupportConverter",
-        imports = arrayOf("co.anitrend.arch.data.converter.SupportConverter")
-    )
-)
 interface ISupportResponseMapper<in S, D> {
 
     /**
      * Creates mapped objects and handles the database operations which may be required to map various objects,
-     * called in [retrofit2.Callback.onResponse] after assuring that the response was a success
      *
      * @param source the incoming data source type
      * @return Mapped object that will be consumed by [onResponseDatabaseInsert]
@@ -25,7 +17,6 @@ interface ISupportResponseMapper<in S, D> {
 
     /**
      * Inserts the given object into the implemented room database,
-     * called in [retrofit2.Callback.onResponse]
      *
      * @param mappedData mapped object from [onResponseMapFrom] to insert into the database
      */
