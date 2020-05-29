471bbf4b4f8d35a8e31fcf4a3e1b858096e4b73a
Maxwell <mxt.developer@gmail.com>
2020-05-29 12:15:30 +0200

    Refactor action mode contract and drop concrete implementation

--- support-recycler/src/main/kotlin/co/anitrend/arch/recycler/action/decorator/ISelectionDecorator.kt
+++ support-recycler/src/main/kotlin/co/anitrend/arch/recycler/action/decorator/ISelectionDecorator.kt
@@ -3,14 +3,11 @@ package co.anitrend.arch.recycler.action.decorator
 import android.view.View
 import android.widget.CheckBox
 import androidx.annotation.DrawableRes
-import androidx.cardview.widget.CardView
-import co.anitrend.arch.extension.getColorFromAttr
-import co.anitrend.arch.extension.getCompatColor
 import co.anitrend.arch.recycler.R
 
 /**
  * A helper interface for applying decorations on sections for
- * [co.anitrend.arch.recycler.action.SupportSelectionMode].
+ * [co.anitrend.arch.recycler.action.contract.ISupportSelectionMode].
  *
  * This interface has a default implementation which can be overridden
  *
@@ -31,16 +28,10 @@ interface ISelectionDecorator {
         @DrawableRes drawableRes: Int = R.drawable.selection_frame
     ) = when (isSelected) {
         true -> when (view) {
-            is CardView -> view.setCardBackgroundColor(
-                view.context.getCompatColor(R.color.colorPrimaryDark)
-            )
             is CheckBox -> view.isChecked = true
             else -> view.setBackgroundResource(drawableRes)
         }
         else -> when (view) {
-            is CardView -> view.setCardBackgroundColor(
-                view.context.getColorFromAttr(R.attr.cardColor)
-            )
             is CheckBox -> view.isChecked = false
             else -> view.setBackgroundResource(0)
         }
