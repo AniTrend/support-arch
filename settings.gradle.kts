f439f0a8f26acf5d054a5c3b5ef9f64285731cf8
Maxwell <mxt.developer@gmail.com>
2020-06-02 17:22:38 +0200

    Convert settings.gradle to settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+include(
+    ":support-core",
+    ":support-ext",
+    ":support-ui",
+    ":support-data",
+    ":support-domain",
+    ":support-theme",
+    ":support-recycler"
+)
+
+/*
+if (System.getenv("CI") == null)
+    include ":app", ":core", ":data", ":domain"
+*/
