3a975e06427b326314a1f43fbc0c9f7ffea98e20
Maxwell <mxt.developer@gmail.com>
2020-06-03 00:06:06 +0200

    Add maven publishing options

--- buildSrc/src/main/java/co/anitrend/arch/buildSrc/plugin/componets/AndroidOptions.kt
+++ buildSrc/src/main/java/co/anitrend/arch/buildSrc/plugin/componets/AndroidOptions.kt
@@ -5,16 +5,27 @@ import org.gradle.api.tasks.bundling.Jar
 import org.gradle.kotlin.dsl.get
 import org.jetbrains.dokka.gradle.DokkaTask
 import co.anitrend.arch.buildSrc.plugin.extensions.baseExtension
+import co.anitrend.arch.buildSrc.plugin.extensions.kotlinAndroidProjectExtension
 import co.anitrend.arch.buildSrc.plugin.extensions.androidExtensionsExtension
 import co.anitrend.arch.buildSrc.plugin.extensions.containsAndroidPlugin
+import co.anitrend.arch.buildSrc.plugin.extensions.publishingExtension
+import co.anitrend.arch.buildSrc.plugin.extensions.libraryExtension
+import co.anitrend.arch.buildSrc.common.Versions
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.kotlin.dsl.getValue
 import java.net.URL
 
 @Suppress("UnstableApiUsage")
 internal fun Project.configureOptions() {
     if (containsAndroidPlugin()) {
+        println("Applying extension options for ${project.path}")
+
         androidExtensionsExtension().isExperimental = true
 
-        println("Applying additional tasks options for ${project.path} -> ${project.projectDir}")
+        val baseExt = baseExtension()
+        val mainSourceSet = baseExt.sourceSets["main"].java.srcDirs
+
+        println("Applying additional tasks options for dokka and javadoc on ${project.path}")
 
         val dokka = tasks.withType(DokkaTask::class.java) {
             outputFormat = "javadoc"
@@ -48,24 +59,22 @@ internal fun Project.configureOptions() {
             }
         }
 
-        val mainSources = baseExtension().sourceSets["main"].java.srcDirs
-
-        val dokkaJar = tasks.create("dokkaJar", Jar::class.java) {
+        val dokkaJar by tasks.register("dokkaJar", Jar::class.java) {
             archiveClassifier.set("javadoc")
             from(dokka)
         }
 
-        val sourcesJar = tasks.create("sourcesJar", Jar::class.java) {
+        val sourcesJar by tasks.register("sourcesJar", Jar::class.java) {
             archiveClassifier.set("sources")
-            from(mainSources)
+            from(mainSourceSet)
         }
 
-        val classesJar = tasks.create("classesJar", Jar::class.java) {
+        val classesJar by tasks.register("classesJar", Jar::class.java) {
             from("${project.buildDir}/intermediates/classes/release")
         }
 
         /*val javadoc = tasks.create("javadoc", Javadoc::class.java) {
-            source = fileTree(mainSources)
+            source = fileTree(mainSourceSet)
             classpath += project.files(extension.bootClasspath.joinToString(File.pathSeparator))
         }*/
 
@@ -80,6 +89,44 @@ internal fun Project.configureOptions() {
             add("archives", classesJar)
             //add("archives", javadocJar)
             add("archives", sourcesJar)
+        }
+
+        afterEvaluate {
+            publishingExtension().publications {
+                libraryExtension().libraryVariants
+                val component = components.findByName("android")
+
+                println("Configuring maven publication options for ${project.path}:maven with component-> ${component?.name}")
+                create("maven", MavenPublication::class.java) {
+                    groupId = "co.anitrend.arch"
+                    artifactId = project.name
+                    version = Versions.versionName
+
+                    artifact(sourcesJar)
+                    artifact("${project.buildDir}/outputs/aar/release/${project.name}.aar")
+                    from(component)
+
+                    pom {
+                        name.set("Support Arch Library")
+                        description.set("A multi-module template library that attempts to make clean arch apps easier to build")
+                        url.set("https://github.com/anitrend/support-arch")
+                        licenses {
+                            license {
+                                name.set("Apache License, Version 2.0")
+                                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                            }
+                        }
+                        developers {
+                            developer {
+                                id.set("wax911")
+                                name.set("Maxwell Mapako")
+                                email.set("mxt.developer@gmail.com")
+                                organizationUrl.set("https://github.com/anitrend")
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
