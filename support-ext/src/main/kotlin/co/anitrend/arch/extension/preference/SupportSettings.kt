a02acb920e68b084ef2ac6330063020f2be30c9f
Maxwell <mxt.developer@gmail.com>
2020-06-03 16:15:37 +0200

    Add preference change flow to broadcast changes when observed

--- support-ext/src/main/kotlin/co/anitrend/arch/extension/preference/SupportSettings.kt
+++ support-ext/src/main/kotlin/co/anitrend/arch/extension/preference/SupportSettings.kt
@@ -4,6 +4,12 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
 import co.anitrend.arch.extension.preference.contract.ISupportSettings
+import co.anitrend.arch.extension.preference.model.PreferenceItem
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.sendBlocking
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 
 /**
  * Core abstract implementation for application preferences
@@ -18,4 +24,20 @@ abstract class SupportSettings(
     constructor(context: Context): this(
         context, PreferenceManager.getDefaultSharedPreferences(context)
     )
+
+    /**
+     * A flow that can be observed to provide changes on preferences as they happen
+     *
+     * @see kotlinx.coroutines.flow.callbackFlow
+     * @see kotlinx.coroutines.flow.collect
+     */
+    @ExperimentalCoroutinesApi
+    val preferenceChangeFlow: Flow<PreferenceItem> =
+        callbackFlow {
+            val listener = SharedPreferences.OnSharedPreferenceChangeListener {
+                    pref, key -> sendBlocking(PreferenceItem(key, pref))
+            }
+            registerOnSharedPreferenceChangeListener(listener)
+            awaitClose { unregisterOnSharedPreferenceChangeListener(listener) }
+        }
 }
