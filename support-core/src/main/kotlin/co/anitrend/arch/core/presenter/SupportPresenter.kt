a9416540b9e117dec43cd0ce1f414cc41b3dae75
Maxwell <mxt.developer@gmail.com>
2020-06-03 16:19:01 +0200

    Remove deprecated presenter properties

--- support-core/src/main/kotlin/co/anitrend/arch/core/presenter/SupportPresenter.kt
+++ support-core/src/main/kotlin/co/anitrend/arch/core/presenter/SupportPresenter.kt
@@ -1,11 +1,9 @@
 package co.anitrend.arch.core.presenter
 
 import android.content.Context
-import android.content.SharedPreferences
 import co.anitrend.arch.core.presenter.contract.ISupportPresenter
 import co.anitrend.arch.extension.lifecycle.SupportLifecycle
 import co.anitrend.arch.extension.preference.SupportSettings
-import co.anitrend.arch.extension.preference.event.OnSharedPreferenceBinder
 
 /**
  * An abstract declaration of what responsibilities a presenter should undertake
@@ -20,25 +18,10 @@ import co.anitrend.arch.extension.preference.event.OnSharedPreferenceBinder
 abstract class SupportPresenter<S : SupportSettings>(
     protected val context: Context,
     override val settings: S
-): ISupportPresenter, OnSharedPreferenceBinder, SupportLifecycle {
+): ISupportPresenter, SupportLifecycle {
 
+    /**
+     * Tag descriptor of the current module
+     */
     override val moduleTag = javaClass.simpleName
-
-    override var isActionModeEnabled = false
-
-    override var offScreenPagerLimit: Int = 3
-
-    /**
-     * Unregister any listeners from fragments or activities
-     */
-    override fun onPause(changeListener: SharedPreferences.OnSharedPreferenceChangeListener) {
-        settings.unregisterOnSharedPreferenceChangeListener(changeListener)
-    }
-
-    /**
-     * Register any listeners from fragments or activities
-     */
-    override fun onResume(changeListener: SharedPreferences.OnSharedPreferenceChangeListener) {
-        settings.registerOnSharedPreferenceChangeListener(changeListener)
-    }
 }
--- support-core/src/main/kotlin/co/anitrend/arch/core/presenter/contract/ISupportPresenter.kt
+++ support-core/src/main/kotlin/co/anitrend/arch/core/presenter/contract/ISupportPresenter.kt
@@ -7,23 +7,8 @@ import co.anitrend.arch.extension.preference.contract.ISupportSettings
  */
 interface ISupportPresenter {
 
+    /**
+     * Contract for settings with base properties
+     */
     val settings: ISupportSettings
-
-    /**
-     * Enables or disables action mode, behaviour should be implemented in your adapter,
-     * in ItemClickLister. Default value for this property is false
-     */
-    @Deprecated("May be removed in 1.3.0-stable when support-recycler module reaches stable")
-    var isActionModeEnabled: Boolean
-
-
-    /**
-     * Indicates the number of pages that should be retained to either side of the
-     * current page in the view hierarchy in an idle state. Pages beyond this
-     * limit will be recreated from the adapter when needed.
-     *
-     * @see [androidx.viewpager.widget.ViewPager.setOffscreenPageLimit]
-     */
-    @Deprecated("May be removed in 1.3.0-stable")
-    var offScreenPagerLimit: Int
 }
