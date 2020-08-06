6c1555d89a9140dc13ba71b8e8031635fccef8da
Maxwell <mxt.developer@gmail.com>
2020-08-06 13:55:53 +0200

    Add dependency management plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+	id("com.github.ben-manes.versions") version "0.29.0"
+}
 
 buildscript {
     repositories {
@@ -24,4 +27,14 @@ tasks {
     val clean by registering(Delete::class) {
         delete(rootProject.buildDir)
     }
+}
+
+tasks.named(
+	"dependencyUpdates",
+	com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask::class.java
+).configure {
+	checkForGradleUpdate = false
+	outputFormatter = "json"
+	outputDir = "build/dependencyUpdates"
+	reportfileName = "report"
 }
