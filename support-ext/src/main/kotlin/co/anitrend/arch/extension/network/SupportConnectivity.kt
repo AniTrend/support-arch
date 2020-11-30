e3cb8eb069d9438e5a14db966df4796f101bf5d6
Maxwell <mxt.developer@gmail.com>
2020-11-30 13:10:03 +0200

    Refactor support connectivity
    Changes implementation to use callback flow instead of lifecycle bound `ISupportLifecycle`

--- support-ext/src/main/kotlin/co/anitrend/arch/extension/network/SupportConnectivity.kt
+++ support-ext/src/main/kotlin/co/anitrend/arch/extension/network/SupportConnectivity.kt
@@ -1,13 +1,11 @@
 package co.anitrend.arch.extension.network
 
-import android.net.ConnectivityManager
-import android.net.Network
-import android.net.NetworkCapabilities
-import android.net.NetworkRequest
-import co.anitrend.arch.extension.lifecycle.SupportLifecycle
+import android.net.*
 import co.anitrend.arch.extension.network.model.ConnectivityState
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.callbackFlow
 
 /**
  * Inspired by [ConnectivityChecker](https://github.com/android/plaid/blob/master/core/src/main/java/io/plaidapp/core/ui/ConnectivityChecker.kt)
@@ -20,8 +18,9 @@ import kotlinx.coroutines.flow.StateFlow
  * @since v1.2.0
  */
 open class SupportConnectivity(
-    private val connectivityManager: ConnectivityManager?
-): SupportLifecycle {
+    private val connectivityManager: ConnectivityManager?,
+    private val connectivityCapabilities: Int = NetworkCapabilities.NET_CAPABILITY_INTERNET
+) {
 
     override val moduleTag: String = SupportConnectivity::class.java.simpleName
 
@@ -34,53 +33,102 @@ open class SupportConnectivity(
     open val isConnected
         get() = (connectivityManager?.allNetworks?.filter {
             val network = connectivityManager.getNetworkCapabilities(it)
-            network?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) ?: false
+            network?.hasCapability(connectivityCapabilities) ?: false
         }?.size ?: 0) > 0
-
-    private val connectedMutableStateFlow =
-        MutableStateFlow<ConnectivityState>(ConnectivityState.Unknown)
 
     /**
      * Connection state flow, allows us to monitor changes on the network connectivity
      *
      * @see ConnectivityState
      */
-    val connectivityState: StateFlow<ConnectivityState> = connectedMutableStateFlow
-
-    private val connectivityCallback =
-        object : ConnectivityManager.NetworkCallback() {
+    val connectivityStateFlow = callbackFlow<ConnectivityState> {
+        val callback = object : ConnectivityManager.NetworkCallback() {
+            /**
+             * Called when the framework connects and has declared a new network ready for use.
+             *
+             *
+             * For callbacks registered with [.registerNetworkCallback], multiple networks may
+             * be available at the same time, and onAvailable will be called for each of these as they
+             * appear.
+             *
+             *
+             * For callbacks registered with [.requestNetwork] and
+             * [.registerDefaultNetworkCallback], this means the network passed as an argument
+             * is the new best network for this request and is now tracked by this callback ; this
+             * callback will no longer receive method calls about other networks that may have been
+             * passed to this method previously. The previously-best network may have disconnected, or
+             * it may still be around and the newly-best network may simply be better.
+             *
+             *
+             * Starting with [android.os.Build.VERSION_CODES.O], this will always immediately
+             * be followed by a call to [.onCapabilitiesChanged]
+             * then by a call to [.onLinkPropertiesChanged], and a call
+             * to [.onBlockedStatusChanged].
+             *
+             *
+             * Do NOT call [.getNetworkCapabilities] or
+             * [.getLinkProperties] or other synchronous ConnectivityManager methods in
+             * this callback as this is prone to race conditions (there is no guarantee the objects
+             * returned by these methods will be current). Instead, wait for a call to
+             * [.onCapabilitiesChanged] and
+             * [.onLinkPropertiesChanged] whose arguments are guaranteed
+             * to be well-ordered with respect to other callbacks.
+             *
+             * @param network The [Network] of the satisfying network.
+             */
             override fun onAvailable(network: Network) {
-                connectedMutableStateFlow.value = ConnectivityState.Connected
+                super.onAvailable(network)
+                offer(ConnectivityState.Connected)
             }
 
+            /**
+             * Called when a network disconnects or otherwise no longer satisfies this request or
+             * callback.
+             *
+             *
+             * If the callback was registered with requestNetwork() or
+             * registerDefaultNetworkCallback(), it will only be invoked against the last network
+             * returned by onAvailable() when that network is lost and no other network satisfies
+             * the criteria of the request.
+             *
+             *
+             * If the callback was registered with registerNetworkCallback() it will be called for
+             * each network which no longer satisfies the criteria of the callback.
+             *
+             *
+             * Do NOT call [.getNetworkCapabilities] or
+             * [.getLinkProperties] or other synchronous ConnectivityManager methods in
+             * this callback as this is prone to race conditions ; calling these methods while in a
+             * callback may return an outdated or even a null object.
+             *
+             * @param network The [Network] lost.
+             */
             override fun onLost(network: Network) {
-                connectedMutableStateFlow.value = ConnectivityState.Disconnected
+                offer(ConnectivityState.Disconnected)
+            }
+
+            /**
+             * Called if no network is found within the timeout time specified in
+             * [.requestNetwork] call or if the
+             * requested network request cannot be fulfilled (whether or not a timeout was
+             * specified). When this callback is invoked the associated
+             * [NetworkRequest] will have already been removed and released, as if
+             * [.unregisterNetworkCallback] had been called.
+             */
+            override fun onUnavailable() {
+                offer(ConnectivityState.Unknown)
             }
         }
 
-    /**
-     * Triggered when the lifecycleOwner reaches it's onPause state
-     *
-     * @see [androidx.lifecycle.LifecycleOwner]
-     */
-    override fun onPause() {
-        super.onPause()
-        connectivityManager?.unregisterNetworkCallback(connectivityCallback)
-    }
-
-    /**
-     * Triggered when the lifecycleOwner reaches it's onResume state
-     *
-     * @see [androidx.lifecycle.LifecycleOwner]
-     */
-    override fun onResume() {
-        super.onResume()
         connectivityManager?.registerNetworkCallback(
             NetworkRequest.Builder()
-                .addCapability(
-                    NetworkCapabilities.NET_CAPABILITY_INTERNET
-                ).build(),
-            connectivityCallback
+                .addCapability(connectivityCapabilities)
+                .build(),
+            callback
         )
+
+        awaitClose {
+            connectivityManager?.unregisterNetworkCallback(callback)
+        }
     }
 }
