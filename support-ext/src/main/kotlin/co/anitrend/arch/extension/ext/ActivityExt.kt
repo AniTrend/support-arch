fd694932e785f97f9eaf33b4e6d0613d1cb58044
Maxwell <mxt.developer@gmail.com>
2020-12-03 22:57:26 +0200

    Re-enable window API decorView flag for pre-R
    Hiding system bars does not seem to work in pre android R when using insets

--- support-ext/src/main/kotlin/co/anitrend/arch/extension/ext/ActivityExt.kt
+++ support-ext/src/main/kotlin/co/anitrend/arch/extension/ext/ActivityExt.kt
@@ -60,24 +60,27 @@ fun FragmentActivity.makeStatusBarTransparent() {
  * Hides both status bar and navigation bar
  */
 fun FragmentActivity.hideStatusBarAndNavigationBar() {
-    //WindowCompat.setDecorFitsSystemWindows(window, true)
-    val controller = ViewCompat.getWindowInsetsController(window.decorView)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+        val controller = ViewCompat.getWindowInsetsController(window.decorView)
 
-    // swipe in system bars, this is now sticky immersive
-    controller?.systemBarsBehavior =
-        WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        // swipe in system bars, this is now sticky immersive
+        controller?.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
 
-    // hide status and navigation bar
-    controller?.hide(WindowInsets.Type.systemBars())
-
-    //window.apply {
-    //    decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or
-    //            View.SYSTEM_UI_FLAG_FULLSCREEN or
-    //            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-    //            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-    //            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-    //            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-    //}
+        // hide status and navigation bar
+        controller?.hide(WindowInsets.Type.systemBars())
+    } else {
+        // Until I figure out how to hide status bar and nav pre android R using insets
+        @Suppress("DEPRECATION")
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LOW_PROFILE or
+            View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
 }
 
 /**
