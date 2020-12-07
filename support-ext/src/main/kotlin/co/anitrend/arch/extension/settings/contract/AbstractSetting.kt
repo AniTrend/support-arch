a73b564e057f0169474f592277c117eee7709913
Maxwell <mxt.developer@gmail.com>
2020-12-07 23:04:01 +0200

    Revert operator overloading for get and set value
    Exposes value property and removes overriden get and set value operators.
    
    Partially reverts: 42ebc8b884763db635e1acc811db3af290c46f6d

--- support-ext/src/main/kotlin/co/anitrend/arch/extension/settings/contract/AbstractSetting.kt
+++ support-ext/src/main/kotlin/co/anitrend/arch/extension/settings/contract/AbstractSetting.kt
@@ -1,9 +1,7 @@
 package co.anitrend.arch.extension.settings.contract
 
 import android.content.SharedPreferences
-import android.content.res.Resources
 import kotlinx.coroutines.flow.Flow
-import kotlin.reflect.KProperty
 
 /**
  * An abstraction of a setting that is both observable and stateful
@@ -24,34 +22,10 @@ abstract class AbstractSetting<T>(
      * Allow you to set a value for the setting or
      * access a snapshot value of the setting
      */
-    protected abstract var value: T
+    abstract var value: T
 
     /**
      * Readonly observable to listen for changes for the setting
      */
     abstract val flow: Flow<T>
-
-    /**
-     * @param thisRef - the object for which the value is requested.
-     * @param property - the metadata for the property.
-     *
-     * @return Snapshot of the setting
-     */
-    operator fun getValue(
-        thisRef: AbstractSetting<T>,
-        property: KProperty<*>
-    ) = thisRef.value
-
-    /**
-     * Sets a value for the setting
-     *
-     * @param thisRef - the object for which the value is requested.
-     * @param property - the metadata for the property.
-     * @param value - the value to set.
-     */
-    operator fun setValue(
-        thisRef: AbstractSetting<T>,
-        property: KProperty<*>,
-        value: T
-    ) { thisRef.value = value }
 }
