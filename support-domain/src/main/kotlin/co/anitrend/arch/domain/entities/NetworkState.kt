d72553deb7f69883f553f5ef2a5aeb0e9314bc3d
Maxwell <mxt.developer@gmail.com>
2021-03-12 13:30:31 +0200

    Deprecate Network State in favour of Load State

--- support-domain/src/main/kotlin/co/anitrend/arch/domain/entities/NetworkState.kt
+++ support-domain/src/main/kotlin/co/anitrend/arch/domain/entities/NetworkState.kt
@@ -1,5 +1,13 @@
 package co.anitrend.arch.domain.entities
 
+@Deprecated(
+    "Replace with LoadState",
+    level = DeprecationLevel.WARNING,
+    replaceWith = ReplaceWith(
+        "LoadState",
+        "co.anitrend.arch.domain.entities.LoadState"
+    )
+)
 /**
  * State representing ongoing, completed or failed requests
  */
@@ -8,16 +16,19 @@ sealed class NetworkState {
     /**
      * Represents a state of idle
      */
+    @Suppress("DEPRECATION")
     object Idle : NetworkState()
 
     /**
      * Represents a network state of loading
      */
+    @Suppress("DEPRECATION")
     object Loading : NetworkState()
 
     /**
      * Represents a network state that has succeeded
      */
+    @Suppress("DEPRECATION")
     object Success : NetworkState()
 
     /**
@@ -27,6 +38,7 @@ sealed class NetworkState {
      * @param heading Heading related to the error message
      * @param message Message to display if any are available
      */
+    @Suppress("DEPRECATION")
     data class Error(
         val code: Int? = null,
         val heading: String? = null,
