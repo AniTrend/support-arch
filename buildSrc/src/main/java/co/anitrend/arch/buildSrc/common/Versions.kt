e2fb4a790eacfef4a1f9bd98eed75791ae77c76e
Maxwell <mxt.developer@gmail.com>
2021-11-14 14:26:30 +0200

    Bump version from 1.3.0-rc6 to 1.3.0

--- buildSrc/src/main/java/co/anitrend/arch/buildSrc/common/Versions.kt
+++ buildSrc/src/main/java/co/anitrend/arch/buildSrc/common/Versions.kt
@@ -13,10 +13,14 @@ internal object Versions {
     private const val major = 1
     private const val minor = 3
     private const val patch = 0
-    private const val revision = 6
+    private const val revision = 0
+
+    private const val channel = "rc"
 
     const val versionCode = major * 100_000 + minor * 10_000 + patch * 1_000 + revision * 100
-    val versionName = "$major.$minor.$patch-rc${revision.toVersion()}"
+    val versionName = if (revision > 0) 
+        "$major.$minor.$patch-rc${revision.toVersion()}"
+    else "$major.$minor.$patch"
 
     const val mockk = "1.12.0"
     const val junit = "4.13.2"
