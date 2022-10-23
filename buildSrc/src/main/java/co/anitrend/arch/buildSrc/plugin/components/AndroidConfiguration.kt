3257feb1afdff8c2fd38ba463cd519a211373558
Maxwell <mxt.developer@gmail.com>
2022-10-23 13:17:06 +0200

    Exclude all license resources from package options
    Adds more coverage for all extension types

--- buildSrc/src/main/java/co/anitrend/arch/buildSrc/plugin/components/AndroidConfiguration.kt
+++ buildSrc/src/main/java/co/anitrend/arch/buildSrc/plugin/components/AndroidConfiguration.kt
@@ -68,9 +68,9 @@ internal fun Project.configureAndroid(): Unit = baseExtension().run {
     }
 
     packagingOptions {
-        resources.excludes.add("META-INF/NOTICE.txt")
+        resources.excludes.add("META-INF/NOTICE.*")
+        resources.excludes.add("META-INF/LICENSE.*")
         resources.excludes.add("META-INF/LICENSE")
-        resources.excludes.add("META-INF/LICENSE.txt")
     }
 
     sourceSets {
