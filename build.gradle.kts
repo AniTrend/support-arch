5c6607296b95cbdddfbfa01ab6ac80f9e026159d
Maxwell Mapako <wax911@users.noreply.github.com>
2026-04-28 23:00:37 +0200

    feat: migrate to Dokka v2, update configurations, and enhance documentation generation (#461)
    * feat: migrate to Dokka v2, update configurations, and enhance documentation generation
    
    * feat: add release-drafter autolabeler workflow and update version updater for dynamic versioning
    
    * fix: correct key names in first-contribution-greeting workflow for consistency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
-
-plugins {
-    id("org.jetbrains.dokka")
-}
-
 buildscript {
     repositories {
         google()
@@ -21,13 +14,4 @@ allprojects {
         google()
         mavenCentral()
     }
-}
-
-tasks.create("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory.get())
-}
-
-tasks.withType(DokkaMultiModuleTask::class.java) {
-    outputDirectory.set(rootProject.file("dokka-docs"))
-    failOnWarning.set(false)
 }
